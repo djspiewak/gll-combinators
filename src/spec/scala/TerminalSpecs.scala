6454344a6a12356ab4b3c95fd69c38f76af2ee56
Daniel Spiewak <djspiewak@gmail.com>
2009-04-14 09:07:41 -0500

    Added preliminary specifications

---
+++ src/spec/scala/TerminalSpecs.scala
@@ -0,0 +1,24 @@
+import edu.uwm.cs.gll._
+import org.specs._
+
+object TerminalSpecs extends Specification with ImplicitConversions {
+  "terminal parsers" should {
+    "parse single tokens" in {
+      val p = literal("test")
+      
+      p("test" toStream) must beLike {
+        case Success("test", Stream()) :: Nil => true
+        case _ => false
+      }
+    }
+    
+    "parse multiple tokens" in {
+      val p = "te" ~ "st"
+      
+      p("test" toStream) must beLike {
+        case Success("te" ~ "st", Stream()) :: Nil => true
+        case _ => false
+      }
+    }
+  }
+}
