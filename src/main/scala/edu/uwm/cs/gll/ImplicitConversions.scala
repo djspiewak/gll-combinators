4ab652e12fc5c7ca755b3adc6b961d48a053f7af
Daniel Spiewak <djspiewak@gmail.com>
2009-04-14 09:07:19 -0500

    Added implicit conversions for literal and disjunctive parsers

---
+++ src/main/scala/edu/uwm/cs/gll/ImplicitConversions.scala
@@ -0,0 +1,17 @@
+package edu.uwm.cs.gll
+
+trait ImplicitConversions {
+  implicit def literal(str: String): Parser[String] = new LiteralParser(str)
+  
+  implicit def disjunctiveSyntax[A](left: =>Parser[A])= new {
+    def |(right: =>Parser[A]): Parser[A] = new DisjunctiveParser(left, right)
+    
+    def |(right: String) = new DisjunctiveParser(left, literal(right))
+  }
+  
+  implicit def disjunctiveLiterals(left: String) = new {
+    def |[A](right: =>Parser[A]) = new DisjunctiveParser(literal(left), right)
+    
+    def |(right: String): Parser[String] = new DisjunctiveParser(literal(left), literal(right))
+  }
+}
