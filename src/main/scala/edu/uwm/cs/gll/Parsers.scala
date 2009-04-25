b48872c2e1b35216fa31c4a47db67f674f64abfe
Daniel Spiewak <djspiewak@gmail.com>
2009-04-25 12:52:37 -0500

    Added convenience aliases for *, + and ?

--- src/main/scala/edu/uwm/cs/gll/Parsers.scala
+++ src/main/scala/edu/uwm/cs/gll/Parsers.scala
@@ -2,4 +2,10 @@ package edu.uwm.cs.gll
 
 trait Parsers extends ImplicitConversions {
   implicit def literal(str: String): Parser[String] = new LiteralParser(str)
+  
+  def opt[A](p: Parser[A]) = p?
+  
+  def rep[A](p: Parser[A]) = p*
+  
+  def rep1[A](p: Parser[A]) = p+
 }
