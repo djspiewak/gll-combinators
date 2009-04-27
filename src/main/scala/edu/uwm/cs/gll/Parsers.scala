3857a08787721f055f28bfce76343c16a5f7d39b
Daniel Spiewak <djspiewak@gmail.com>
2009-04-27 15:35:08 -0500

    Implemented some basic optimizations for popped and backlinks

--- src/main/scala/edu/uwm/cs/gll/Parsers.scala
+++ src/main/scala/edu/uwm/cs/gll/Parsers.scala
@@ -464,18 +464,23 @@ trait Parsers {
     private type Potential = (Parser[Any], Stream[Char])
     
     private val queue = new mutable.Queue[Potential]
-    private val backlinks = mutable.Map[Potential, mutable.ListBuffer[Result[Any]=>Unit]]()
-    private val popped = mutable.Map[Potential, mutable.ListBuffer[Result[Any]]]()
+    private val backlinks = mutable.Map[Stream[Char], mutable.Map[Parser[Any], mutable.ListBuffer[Result[Any]=>Unit]]]()
+    private val popped = mutable.Map[Potential, mutable.Set[Result[Any]]]()
     
     def run() {
       while (queue.length > 0) {
         val (p, s, f) = pop()
         
         p.queue(this, s) { res =>
-          popped((p, s)) += res             // store the result for late-comers
-          f(res)
+          val tuple = (p, s)
           
-          tracef("Saved: %s *=> %s%n", (p, s), res)
+          if (!popped.contains(tuple))
+            popped += (tuple -> new mutable.HashSet[Result[Any]])
+          
+          popped(tuple) += res
+          tracef("Saved: %s *=> %s%n", tuple, res)
+          
+          f(res)
         }
       }
     }
@@ -483,17 +488,20 @@ trait Parsers {
     def push[A](p: Parser[A], s: Stream[Char])(f: Result[A]=>Unit) {
       val tuple = (p, s)
       
-      if (popped.contains(tuple) && popped(tuple).size > 0) {
+      if (popped contains tuple) {
         for (res <- popped(tuple)) {           // if we've already done that, use the result
           tracef("Revisited: %s *=> %s%n", tuple, res)
           f(res.asInstanceOf[Result[A]])
         }
       } else {
-        if (!backlinks.contains(tuple)) {
+        if (!backlinks.contains(s))
+          backlinks += (s -> new mutable.HashMap[Parser[Any], mutable.ListBuffer[Result[Any]=>Unit]]())
+        
+        if (!backlinks(s).contains(p)) {
           queue += tuple
-          backlinks += (tuple -> new ListBuffer[Result[Any]=>Unit])
+          backlinks(s) += (p -> new ListBuffer[Result[Any]=>Unit])
         }
-        backlinks(tuple) += f.asInstanceOf[Result[Any]=>Unit]
+        backlinks(s)(p) += f.asInstanceOf[Result[Any]=>Unit]
   
         trace("Pushed: " + tuple)
       }
@@ -502,7 +510,7 @@ trait Parsers {
     private def pop() = {
       val tuple @ (p, s) = queue.dequeue()
       val f = {
-        val links = backlinks(tuple)
+        val links = backlinks(s)(p)
         
         if (links.size == 1)
           links.toList.head
@@ -510,8 +518,9 @@ trait Parsers {
           { res: Result[Any] => links foreach { _(res) } }
       }
       
-      backlinks -= tuple
-      popped += (tuple -> new ListBuffer[Result[Any]])
+      backlinks(s) -= p
+      if (backlinks(s).size == 0)
+        backlinks -= s
   
       trace("Popped: " + tuple)
       
