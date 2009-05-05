aa03224d23e3c16bb1842cfdd84fbc1a4bbc040a
Daniel Spiewak <djspiewak@gmail.com>
2009-05-05 12:35:05 -0500

    Should have been unapplySeq

--- src/main/scala/edu/uwm/cs/gll/LineStream.scala
+++ src/main/scala/edu/uwm/cs/gll/LineStream.scala
@@ -156,7 +156,7 @@ object LineStream {
     gen(src.getLines, 1)
   }
   
-  def unapply(str: LineStream) = Some(str)
+  def unapplySeq(str: LineStream) = Some(str)
 }
 
 class LineCons(val head: Char, _tail: =>LineStream, line: String, lineNum: Int) extends LineStream(constructLine(line, head, _tail), constructLineNum(lineNum, head)) {
