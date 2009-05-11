903906fcd08a9d7b87efad14175d5565385cf548
Daniel Spiewak <djspiewak@gmail.com>
2009-05-10 23:09:41 -0500

    Added configurability to whitespace skipping

--- src/main/scala/edu/uwm/cs/gll/RegexParsers.scala
+++ src/main/scala/edu/uwm/cs/gll/RegexParsers.scala
@@ -7,26 +7,41 @@ import util._
 // TODO need to handle trailing whitespace (somehow)
 trait RegexParsers extends Parsers {
   protected val whitespace = """\s+"""r
+  protected val skipWhitespace = true
   
-  override implicit def literal(str: String): Parser[String] = new LiteralParser(str) {
-    override def computeFirst(seen: Set[Parser[Any]]) = Some(UniversalOptCharSet)    // because of whitespace
-    
-    // there should be a way to do this with traits, but I haven't found it yet
-    override def parse(s: LineStream) = super.parse(handleWhitespace(s))
+  override implicit def literal(str: String): Parser[String] = {
+    if (skipWhitespace) {
+      new LiteralParser(str) {
+        override def computeFirst(seen: Set[Parser[Any]]) = Some(UniversalOptCharSet)    // because of whitespace
+        
+        // there should be a way to do this with traits, but I haven't found it yet
+        override def parse(s: LineStream) = super.parse(handleWhitespace(s))
+      }
+    } else super.literal(str)
   }
   
-  implicit def regex(r: Regex): Parser[String] = new RegexParser(r) {
-    override def computeFirst(seen: Set[Parser[Any]]) = Some(UniversalOptCharSet)
-    
-    override def parse(s: LineStream) = super.parse(handleWhitespace(s))
+  implicit def regex(r: Regex): Parser[String] = {
+    if (skipWhitespace) {
+      new RegexParser(r) {
+        override def computeFirst(seen: Set[Parser[Any]]) = Some(UniversalOptCharSet)
+        
+        override def parse(s: LineStream) = super.parse(handleWhitespace(s))
+      }
+    } else new RegexParser(r)
   }
   
   implicit def disjunctiveRegex(left: Regex) = new RichParser(regex(left))
   
   implicit def funRegexSyntax(p: Regex) = new RichSyntax1(regex(p))
   
-  override protected def processTail(tail: LineStream) = 
-    super.processTail(handleWhitespace(tail))
+  override protected def processTail(tail: LineStream) = {
+    val newTail = if (skipWhitespace)
+      handleWhitespace(tail)
+    else
+      tail
+    
+    super.processTail(newTail)
+  }
   
   private def handleWhitespace(s: LineStream) =
     s.drop(whitespace findPrefixOf s map { _.length } getOrElse 0)
