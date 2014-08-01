27931c9c0c52eff557251f0e4e4bcdc2d244f893
Daniel Spiewak <djspiewak@gmail.com>
2014-07-31 23:31:54 -0600

    Corrected optimized disjunction of regex parsers with a regex on the RHS (#22)

--- src/main/scala/com/codecommit/gll/RegexParsers.scala
+++ src/main/scala/com/codecommit/gll/RegexParsers.scala
@@ -6,10 +6,10 @@ import scala.util.matching.Regex
 
 trait RegexParsers extends Parsers {
   import SetSyntax._
-  
+
   protected val whitespace = """\s+"""r
   protected val skipWhitespace = true
-  
+
   override implicit def literal(str: String): LiteralParser = {
     if (skipWhitespace) {
       new LiteralParser(str) {
@@ -20,21 +20,21 @@ trait RegexParsers extends Parsers {
       }
     } else super.literal(str)
   }
-  
+
   implicit def regex(r: Regex): RegexParser =
     new RegexParser(r)
-  
+
   override implicit def disjunctiveLiterals(left: String): RichRegexParser =
     new RichRegexParser(regex(new Regex(escapeRegex(left))))
-  
+
   implicit def disjunctiveRegex(left: Regex): RichRegexParser =
     new RichRegexParser(left)
-  
+
   implicit def funRegexSyntax(p: Regex) = new RichSyntax1(regex(p))
-  
+
   private def escapeRegex(str: String) = {
     val specialChars = Set('[', ']', '{', '}', '\\', '|', '*', '+', '?', '^', '$', '(', ')')
-    
+
     augmentString(str).foldLeft("") { (str, c) =>
       if (specialChars contains c)
         str + '\\' + c
@@ -48,42 +48,42 @@ trait RegexParsers extends Parsers {
       s.drop(whitespace findPrefixOf s map { _.length } getOrElse 0)
     else
       s
-  
-  
+
+
   class RichRegexParser(left: RegexParser) extends RichParser(left) {
-    def |(right: Regex) = new RegexParser(new Regex("(" + left.regex.toString + ")|(" + escapeRegex(right.regex.toString) + ")"))
-    
+    def |(right: Regex) = new RegexParser(new Regex("(" + left.regex.toString + ")|(" + right.regex.toString + ")"))
+
     def |(right: String) = new RegexParser(new Regex("(" + left.regex.toString + ")|(" + escapeRegex(right) + ")"))
   }
-  
+
   case class RegexParser(val regex: Regex) extends TerminalParser[String] {
     if (regex == null)
       throw new NullPointerException("Cannot parse a null regular expression")
-    
+
     lazy val constFirst = {
       val wsFirst = if (skipWhitespace) RegexUtils.first(whitespace) else Set[Option[Char]]()
       val regexFirst = RegexUtils.first(regex)
-      
+
       if (wsFirst.isComplement)
         Some((wsFirst - None) ++ regexFirst)
       else
         Some(regexFirst ++ (wsFirst - None))
     }
-    
+
     def computeFirst(s: Set[Parser[Any]]) = constFirst
-    
+
     def parse(in: LineStream) = {
       val res = regex findPrefixOf in map { str => Success(str, in drop str.length) }
       res getOrElse Failure(ExpectedRegex(regex), in)
     }
-    
+
     override def equals(other: Any) = other match {
       case that: RegexParser => this.regex.toString == that.regex.toString
       case _ => false
     }
-    
+
     override def hashCode = regex.toString.hashCode
-    
+
     override def toString = "/%s/".format(regex)
   }
 }
--- src/test/scala/RegexSpecs.scala
+++ src/test/scala/RegexSpecs.scala
@@ -10,23 +10,23 @@ object RegexSpecs extends Specification
     with NoTildeSyntax
     with ScalaCheck
     with RegexParsers {
-  
+
   import Prop._
   import StreamUtils._
-  
+
   "regex parsers" should {
     "match, consume and return from a regex match" in {
       {
         val p: Parser[String] = """(\d{1,3}\.){3}\d{1,3}"""r
-        
+
         p("192.168.101.2") must beLike {
           case Success("192.168.101.2", LineStream()) #:: SNil => ok
         }
       }
-      
+
       {
         val p: Parser[String] = """\d+"""r
-        
+
         p("1234daniel") must beLike {
           case Failure(UnexpectedTrailingChars("daniel"), LineStream('d', 'a', 'n', 'i', 'e', 'l')) #:: SNil => ok
         }
@@ -44,43 +44,43 @@ object RegexSpecs extends Specification
         case Success(")", LineStream()) #:: SNil => ok
       }
     }
-    
+
     "match correctly when using a boundary token" in {
       val p = "a" ~ "where\\b".r ~ "b"
-      
+
       p("a where b") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
-      
+
       p("a whereb") must beLike {
         case Failure(_, _) #:: SNil => ok
       }
     }
-    
+
     "gracefully error on null regexp" in {
       regex(null) must throwA(new NullPointerException("Cannot parse a null regular expression"))
     }
-    
+
     "produce 'expected' error message" in {
       val rex = """(\d{1,3}\.){3}\d{1,3}"""r
       val p: Parser[String] = rex
-      
+
       {
         val data = LineStream("123.457.321")
-        
+
         p(data) must beLike {
           case Failure(ExpectedRegex(`rex`), `data`) #:: SNil => ok
         }
       }
-      
+
       {
         val data = LineStream("123.457.321.sdf")
-        
+
         p(data) must beLike {
           case Failure(ExpectedRegex(`rex`), `data`) #:: SNil => ok
         }
       }
-      
+
       {
         p(LineStream()) must beLike {
           case Failure(ExpectedRegex(`rex`), LineStream()) #:: SNil => ok
@@ -99,7 +99,7 @@ object RegexSpecs extends Specification
           l.toString mustEqual "daniel"
       }
     }
-    
+
     "produce a location of after leading whitespace in seqential parser" in {
       sealed trait A
       case class B(loc: LineStream, id: String, left: String, right: A) extends A
@@ -119,98 +119,98 @@ object RegexSpecs extends Specification
 
     "eat leading whitespace" in {
       val p = literal("daniel")
-      
+
       p("daniel") must beLike {
         case Success("daniel", LineStream()) #:: SNil => ok
       }
-      
+
       p("  daniel") must beLike {
         case Success("daniel", LineStream()) #:: SNil => ok
       }
-      
+
       p("\tdaniel") must beLike {
         case Success("daniel", LineStream()) #:: SNil => ok
       }
-      
+
       p("""
       daniel""") must beLike {
         case Success("daniel", LineStream()) #:: SNil => ok
       }
     }
-    
+
     "eat trailing whitespace" in {
       val p = literal("daniel")
-      
+
       p("daniel    ") must beLike {
         case Success("daniel", LineStream()) #:: SNil => ok
       }
-      
+
       p("daniel\t") must beLike {
         case Success("daniel", LineStream()) #:: SNil => ok
       }
-      
+
       p("daniel\n") must beLike {
         case Success("daniel", LineStream()) #:: SNil => ok
       }
     }
-      
+
     "eat infix whitespace" in {
       val num = """\d+""".r
       val p = num ~ "+" ~ num
-      
+
       p("1 + 2") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
-      
+
       p("1 +2") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
-      
+
       p("1+ 2") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
-      
+
       p("  1   +  2") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
     }
-      
+
     "eat infix whitespace with explicit type attribution" in {
       val num: Parser[String] = """\d+""".r
       val p = num ~ "+" ~ num
-      
+
       p("1 + 2") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
-      
+
       p("1 +2") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
-      
+
       p("1+ 2") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
-      
+
       p("  1   +  2") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
     }
-    
+
     "eat infix whitespace without disrupting subsquent regex" in {
       val num = """[0-9]+(\.[0-9]+)?([eE][0-9]+)?""".r
       val str = """"([^\n\r\\]|\\.)*"""".r
-      
+
       lazy val expr: Parser[Any] = (
           expr ~ "+" ~ expr
         | num
         | str
       )
-      
+
       expr("1 + \"a\"") must beLike {
         case Success(_, LineStream()) #:: _ => ok
       }
     }
-    
+
     "define FIRST set" in {
       """\d""".r.first must containAllOf(List('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'))
       "abc".r.first must contain('a')
@@ -220,43 +220,51 @@ object RegexSpecs extends Specification
       ("[^abc]".r.first == new ComplementarySet(Set('a', 'b', 'c'))) mustEqual true
       ("abc|(def)|[^abc]".r.first == new ComplementarySet(Set('b', 'c'))) mustEqual true
     }
-    
+
     "negate using a regexp parser" in {
       val p1 = "test" \ ("test" | "ing")
-      
+
       p1("test") must beLike {
         case Failure(SyntaxError, LineStream(tail @ _*)) #:: SNil =>
           tail.mkString mustEqual "test"
       }
-      
+
       val p2 = "test" \ ("blah" | "ing ")
-      
+
       p2("test") must beLike {
         case Success("test", LineStream()) #:: SNil => ok
       }
-      
+
       p2("ing ") must beLike {
         case Failure(ExpectedLiteral("test", "ing "), LineStream(tail @ _*)) #:: SNil =>
           tail.mkString mustEqual "ing "
       }
-      
+
       p2("blah") must beLike {
         case Failure(ExpectedLiteral("test", "blah"), LineStream(tail @ _*)) #:: SNil =>
           tail.mkString mustEqual "blah"
       }
     }
-    
+
     "correctly globally disambiguate a local sequence ambiguity" in {
       lazy val expr: Parser[Any] = (
           id ~ ":=" ~ expr ~ expr
         | num
         | id
       )
-      
+
       lazy val id = """[a-zA-Z][a-zA-Z0-9_]*""".r
       lazy val num = """\d+""".r
-      
+
       expr("a := 1 c := 2 3") must beLike {
+        case Success(_, LineStream()) #:: SNil => ok
+      }
+    }
+
+    "not double-escape a disjuncted regex on the RHS" in {
+      val pat = "_" | """[a-z]+""".r
+
+      pat("long") must beLike {
         case Success(_, LineStream()) #:: SNil => ok
       }
     }
