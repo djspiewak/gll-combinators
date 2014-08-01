6cd491dec5cfa73e4439444e58e8ab8363f87af3
Daniel Spiewak <djspiewak@gmail.com>
2014-07-31 22:51:27 -0600

    Fixed StackOverflowError on lengthy input (#21)

--- src/main/scala/com/codecommit/gll/LineStream.scala
+++ src/main/scala/com/codecommit/gll/LineStream.scala
@@ -2,6 +2,7 @@ package com.codecommit.gll
 
 import java.io.{PrintStream, StringReader, Reader}
 
+import scala.annotation.tailrec
 import scala.collection.LinearSeq
 import scala.io.Source
 
@@ -10,12 +11,21 @@ import scala.io.Source
  * amortized constant-time {@link CharSequence} access.
  */
 sealed abstract class LineStream(val line: String, val lineNum: Int, val colNum: Int) extends LinearSeq[Char] with CharSequence { outer =>
+
+  final lazy val length = _length(0)
+
+  @tailrec
+  private def _length(acc: Int): Int = this match {
+    case _: LazyLineCons | _: StrictLineCons => tail._length(acc + 1)
+    case LineNil => acc
+  }
+
   override def tail: LineStream = sys.error("yeah, this is annoying")
-  
+
   def charAt(i: Int) = apply(i)
-  
+
   def subSequence(start: Int, end: Int) = this take end drop start
-  
+
   override def take(length: Int): LineStream = {
     if (length < 0)
       throw new IllegalArgumentException(length.toString)
@@ -24,7 +34,7 @@ sealed abstract class LineStream(val line: String, val lineNum: Int, val colNum:
     else
       LineNil
   }
-  
+
   override def drop(length: Int): LineStream = {
     if (length < 0)
       throw new IllegalArgumentException(length.toString)
@@ -35,7 +45,7 @@ sealed abstract class LineStream(val line: String, val lineNum: Int, val colNum:
     else
       throw new IndexOutOfBoundsException(length.toString)
   }
-  
+
   override def lengthCompare(i: Int) = {
     if (isEmpty)
       -i
@@ -44,14 +54,14 @@ sealed abstract class LineStream(val line: String, val lineNum: Int, val colNum:
     else
       tail lengthCompare i - 1
   }
-  
+
   def zip(that: LineStream): Stream[(Char, Char)] = {
     if (this.isEmpty || that.isEmpty)
       Stream.empty
     else
       Stream.cons((this.head, that.head), tail zip that)
   }
-  
+
   def zipWithIndex = {
     def gen(tail: LineStream, i: Int): Stream[(Char, Int)] = {
       if (tail.isEmpty)
@@ -59,15 +69,15 @@ sealed abstract class LineStream(val line: String, val lineNum: Int, val colNum:
       else
         Stream.cons((tail.head, i), gen(tail.tail, i + 1))
     }
-    
+
     gen(this, 0)
   }
-  
+
   override def toString = mkString
-  
+
   /**
    * Expects a pattern with the following arguments:
-   * 
+   *
    * <ol>
    *   <li><code>%d</code> &mdash; Line number</li>
    *   <li><code>%s</code> &mdash; Line contents</li>
@@ -77,10 +87,10 @@ sealed abstract class LineStream(val line: String, val lineNum: Int, val colNum:
   def printError(pattern: String)(ps: PrintStream) {
     ps.print(formatError(pattern))
   }
-  
+
   /**
    * Expects a pattern with the following arguments:
-   * 
+   *
    * <ol>
    *   <li><code>%d</code> &mdash; Line number</li>
    *   <li><code>%s</code> &mdash; Line contents</li>
@@ -91,71 +101,71 @@ sealed abstract class LineStream(val line: String, val lineNum: Int, val colNum:
     val caret = (1 to colNum).foldLeft("") { (acc, _) => acc + ' ' } + '^'
     pattern.format(lineNum, line, caret)
   }
-  
+
   override def equals(a: Any) = a match {
     case that: LineStream => this eq that
     case _ => false
   }
-  
+
   override def hashCode = lineNum * colNum
 }
 
 object LineStream {
   val empty: LineStream = LineNil
-  
+
   def apply(chars: Char*): LineStream = apply(new String(chars.toArray))
-  
+
   def apply(str: String): LineStream = apply(new StringReader(str))
-  
+
   def apply(itr: Iterator[Char]): LineStream = {
     apply(new Reader {
       def close() {}
-      
+
       def read(cbuf: Array[Char], off: Int, len: Int) = {
         val emptyAtStart = itr.isEmpty
         val proj = itr take len
-        
+
         var count = 0
         while (proj.hasNext) {
           cbuf(count) = proj.next()
           count += 1
         }
-        
+
         if (emptyAtStart) -1 else count
       }
     })
   }
-  
+
   def apply(reader: Reader): LineStream = {
     def gen(num: Int): LineStream = {
       def state0(acc: StringBuilder): (String, String) = {
         val c = reader.read()
-        
-        if (c < 0) 
+
+        if (c < 0)
           (acc.toString, "")
-        else if (c == '\n') 
+        else if (c == '\n')
           (acc.toString, "\n")
-        else if (c == '\r') 
+        else if (c == '\r')
           state1(acc)
-        else 
+        else
           state0(acc.append(c.toChar))
       }
-      
+
       def state1(acc: StringBuilder): (String, String) = {
         val c = reader.read()
-        
-        if (c < 0) 
+
+        if (c < 0)
           (acc.append('\r').toString, "")
-        else if (c == '\n') 
+        else if (c == '\n')
           (acc.toString, "\r\n")
-        else if (c == '\r') 
+        else if (c == '\r')
           state1(acc.append('\r'))
-        else 
+        else
           state0(acc.append('\r').append(c.toChar))
       }
-      
+
       val (line, term) = state0(new StringBuilder)
-      
+
       // cannot use a fold because we don't have a non-strict foldRight
       val termLS = if (term.length == 0)
         LineNil
@@ -165,46 +175,40 @@ object LineStream {
         new StrictLineCons(term(0), new LazyLineCons(term(1), gen(num + 1), line, num, line.length + 2), line, num, line.length + 1)
       else
         sys.error("Line terminator contains more than two characters; cannot process newline!")
-      
+
       val (back, _) = line.foldRight((termLS, line.length)) {
         case (c, (tail, colNum)) => (new StrictLineCons(c, tail, line, num, colNum), colNum - 1)
       }
-      
+
       back
     }
-    
+
     gen(1)
   }
-  
+
   def unapplySeq(str: LineStream): Option[Seq[Char]] = Some(str)
 }
 
 class LazyLineCons(override val head: Char, _tail: =>LineStream, line: String, lineNum: Int, colNum: Int) extends LineStream(line, lineNum, colNum) {
   override lazy val tail = _tail
-  
-  override lazy val length = 1 + tail.length
-  
+
   override val isEmpty = false
-  
+
   def apply(i: Int) = if (i == 0) head else tail(i - 1)
 }
 
 class StrictLineCons(override val head: Char, override val tail: LineStream, line: String, lineNum: Int, colNum: Int) extends LineStream(line, lineNum, colNum) {
-  override lazy val length = 1 + tail.length
-  
   override val isEmpty = false
-  
+
   def apply(i: Int) = if (i == 0) head else tail(i - 1)
 }
 
 object LineNil extends LineStream("", 1, 1) {
-  override val length = 0
-  
   override val isEmpty = true
-  
+
   def apply(i: Int) = throw new StringIndexOutOfBoundsException("Cannot index into an empty LineStream")
-  
+
   override def head = throw new RuntimeException("Cannot get the head of an empty LineStream")
-  
+
   override def tail = throw new RuntimeException("Cannot get the tail of an empty LineStream")
 }
--- src/test/scala/LineStreamSpecs.scala
+++ src/test/scala/LineStreamSpecs.scala
@@ -8,44 +8,45 @@ import org.specs2.mutable._
 import org.scalacheck._
 
 object LineStreamSpecs extends Specification with ScalaCheck {
+  import Function._
   import Prop._
-  
+
   "LineStream" should {
     "have length for String construction" in check { str: String =>
       LineStream(str).length mustEqual str.length
     }
-    
+
     "have length for Source construction" in check { str: String =>
       LineStream(Source.fromString(str)).length mustEqual str.length
     }
-    
+
     "have length for Char* construction" in check { str: String =>
       LineStream(str: _*).length mustEqual str.length
     }
-    
+
     "correctly define apply()" in check { (i: Int, str: String) =>
       (str.length > 0) ==> {
         val idx = abs(i % str.length)
         val stream = LineStream(str)
-        
+
         stream(0) mustEqual stream.head
         stream(idx) mustEqual (str charAt idx)
       }
     }
-    
+
     "define equality by identity and not contents" in {
       val a = new LazyLineCons('a', sys.error("clever test failure"), "a", 1, 1)
       val b = new LazyLineCons('a', sys.error("clever test failure"), "a", 1, 1)
-      
+
       (a == b) mustEqual false
       (a == a) mustEqual true
       (b == b) mustEqual true
     }
-    
+
     "define hashCode by identity" in {
       new LazyLineCons('a', sys.error("clever test failure"), "a", 1, 1).hashCode mustEqual 1
     }
-    
+
     "define a different lineNum/colNum pair for each index" in {
       def allNums(ls: LineStream): Stream[(Int, Int)] = {
         if (ls.isEmpty)
@@ -53,13 +54,19 @@ object LineStreamSpecs extends Specification with ScalaCheck {
         else
           (ls.lineNum, ls.colNum) #:: allNums(ls.tail)
       }
-      
+
       check { str: String =>
         val ls = LineStream(str)
         Set(allNums(ls): _*) must haveSize(str.length)
       }
     }
+
+    "not throw SOE on large input" in {
+      val str = (0 until 1000000) map const('a') mkString "\n"
+
+      LineStream(str).length must not(throwA[StackOverflowError])
+    }
   }
-  
+
   def abs(i: Int) = math.abs(i)
 }
